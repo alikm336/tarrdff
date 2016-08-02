01d23ca22c6c92eddebaa2bbdb56b3075c74e2dd
alikm336 <nextplusk1@gmail.com>
2016-08-02 09:43:29 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {189098208},
+    sudo_users = {!189098208},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
